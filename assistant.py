6a3159e58288ff063d54b90810b1796c006301f8
oliverbond93 <78548510+oliverbond93@users.noreply.github.com>
2021-11-07 23:40:33 +0000

    Random Function
    adds a function which prints off a random number of numbers

--- assistant.py
+++ assistant.py
@@ -1,7 +1,16 @@
 # Some random placeholder python file for the assistant
 
+import random
+
 print('Hello World')
 
 print('blablablablablablalbalblalba')
 
 print('Test of Pycharm git stuff')
+
+def what_is_this():
+  
+  for i in range(1,random.randint(1,200)):
+    print(i)
+    
+what_is_this()
